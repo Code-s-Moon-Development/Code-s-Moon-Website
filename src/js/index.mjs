83829b4f8cb9ca497c6b3a2e4d3f547ca496d98b
Rafael Ribeiro <cortadordeunha666@gmail.com>
2021-08-20 11:38:27 -0300

    refactor: import functions; create callback functions and add event listeners

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,4 +1,22 @@
+import {
+  scheduleAnimations,
+  applyAnimationsWhenThePageLoads,
+} from './events/applyAnimations.mjs';
 import defineImagesSizes from './events/defineImagesSizes.mjs';
 import toggleShowHamburguerMenu from './events/toggleShowHamburguerMenu.mjs';
 
 defineImagesSizes();
+
+function callbackFunctionForLoadedPage() {
+  setTimeout(applyAnimationsWhenThePageLoads, 75);
+  defineImagesSizes();
+}
+
+function callbackFunctionForResizing() {
+  scheduleAnimations();
+  defineImagesSizes();
+}
+
+window.addEventListener('load', callbackFunctionForLoadedPage);
+
+window.addEventListener('resize', callbackFunctionForResizing);
